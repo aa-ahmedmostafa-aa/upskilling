13cb2ae972420e77b1b3de60a3d95645ce93e3e3
aa-ahmedmostafa-aa <ahmed.moustafa9663@gmail.com>
2024-01-07 14:46:51 +0200

    build ads module

--- app.js
+++ app.js
@@ -64,7 +64,11 @@ app.use(helmet());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/uploads", cors(), express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", cors(), express.static(path.join(__dirname, "uploads")), {
+  setHeaders: (res) => {
+    res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  },
+});
 
 // jobs()
 
